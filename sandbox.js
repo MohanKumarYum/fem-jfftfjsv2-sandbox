ab1ee809dfb3c78337a81c0ab88c15a163b08117
Mohan Kumar <MohanKumarYum@Gmail.com>
2018-10-21 08:10:58 +1100

    Declaring objects and intialising different ways.

---
+++ sandbox.js
@@ -0,0 +1,17 @@
+// declare a person object and initialising it separately.
+
+var person = {};
+
+person.name = "Mrs. White";
+
+person.name;
+
+console.log(person.name);
+
+// declaration and initialisation of object in one statement.
+
+var person = {
+    "name":"Mrs. White"
+};
+
+console.log(person.name);
