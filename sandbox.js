a98f490a49f00df563a9b30a4c92ceb3feaadfa9
Mohan Kumar <MohanKumarYum@Gmail.com>
2018-10-22 09:06:06 +1100

    Another variation to accessing array elements in an object.

--- sandbox.js
+++ sandbox.js
@@ -4,8 +4,10 @@ var person = {}
 
 person[0] = {};
 
-console.log(person); // { '0': {} }
+person = {newStr:"I am in the object."};
 
-console.log(person[0]); // {}
+console.log(person); // { newStr: 'I am in the object.' }
 
-console.log(person['0']); // {}
+console.log(person[0]); // undefined
+
+console.log(person['0']); // undefined
