8aea66700e6d4dde3d9c176a4ad51f6d62928254
Mohan Kumar <MohanKumarYum@Gmail.com>
2018-10-22 09:04:14 +1100

    Accessing an object with array elements.

--- sandbox.js
+++ sandbox.js
@@ -1,26 +1,11 @@
 // declare a person array and initializing it separately.
 
-var person = [];
+var person = {}
 
-var plea = "wouldShe";
+person[0] = {};
 
-person.name = "Mrs. White";
+console.log(person); // { '0': {} }
 
-person[plea] = "I would never!"; // using a variable instead of a number, you can use an object or another function with the square brackets.
+console.log(person[0]); // {}
 
-console.log(person); // [ name: 'Mrs. White', 'wouldShe': 'I would never!' ]
-
-person["digit"] = "Zero property name"; // cannot coerce to string if it starts with a number.
-
-console.log(person.digit); // [ name: 'Mrs. White', 'wouldShe': 'I would never!' ]
-
-person["0digit"] = "Zero property name"; // coerce to string if it starts with a number.
-
-console.log(person); //         [   name: 'Mrs. White',
-                                // wouldShe: 'I would never!',
-                                // digit: 'Zero property name',
-                                // '0digit': 'Zero property name' ]
-
-console.log(person.'0digit'); // errors out - since the compiler cannot evaluate the property "0digit".
-
-console.log(person['0digit']); // Zero property name
+console.log(person['0']); // {}
