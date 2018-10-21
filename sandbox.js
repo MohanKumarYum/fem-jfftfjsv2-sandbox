4156e9e7e57d2c8b2893fd3659a1d0a5b9ff3fc6
Mohan Kumar <MohanKumarYum@Gmail.com>
2018-10-22 05:05:10 +1100

    using a string or number that is not yet declared - return's undefined.

--- sandbox.js
+++ sandbox.js
@@ -4,22 +4,8 @@ var person = [];
 
 person.name = "Mrs. White";
 
-// Primitive types are pass by value. - String, number, boolean, true, false, Undefined & null
-// assigning the "name" property of the person object to a variable named who.
+person[plea] = "I would never!";
 
-var who = person.name;
+console.log(person); // undefined as "plea" is not defined.
 
-console.log(who); // "Mrs. White"
 
-console.log(typeof(person) === "array"); // equates to false - person variable defined as an array is an object.
-
-console.log(typeof(person) === "object"); // true - person is an object.
-
-// re-assign a new value to name property of the person object.
-
-person.name = "Mr. White";
-
-// the value of variable who, remains unchanged even after re-assigning the name property.
-console.log(who); // "Mrs. White"
-
-// Non-primitive types are store/pass by reference - Object, array, functions, promise......
