8ebf6f01ebd4a263719ada5feef53fec40109125
Mohan Kumar <MohanKumarYum@Gmail.com>
2018-10-22 05:11:15 +1100

    Array's accessed by passing objects, functions....

--- sandbox.js
+++ sandbox.js
@@ -2,10 +2,12 @@
 
 var person = [];
 
+var plea = "wouldShe";
+
 person.name = "Mrs. White";
 
-person[plea] = "I would never!";
+person[plea] = "I would never!"; // using a variable instead of a number, you can use an object or another function with the square brackets.
 
-console.log(person); // undefined as "plea" is not defined.
+console.log(person); // [ name: 'Mrs. White', 'wouldShe': 'I would never!' ]
 
-
+console.log(person.wouldShe);
