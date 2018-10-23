78e22476a9d06bbb5c2a930bf35e1e694bf9ba5e
Mohan Kumar <MohanKumarYum@Gmail.com>
2018-10-24 07:00:57 +1100

    Creative problem of characters, draft solution.

--- sandbox.js
+++ sandbox.js
@@ -1,63 +1,26 @@
-// non-valid characters in an array
+// Create an object using bracket and dot notation  that represents the characters and related data you may find in a game of clue.
 
-var box = {};
+var game = {};
 
-box["material"] = "cardboard";
-box[0] = "meow";
-box["^&*"] = "testing 123";
+//{ murder: '???', 
+//weapons: [ 'lasers', 'angry cats', 'dish soap' ],
+//name: [ 'Miss Scarlet', 'Mr. Green' ] }
 
-var test = box["^&*"];
+// converting object notation for game to array - square brackets
+// var game = [];
 
-var x = {};
+//[ murder: '???',
+//  weapons: [ 'lasers', 'angry cats', 'dish soap' ],
+//  name: [ 'Miss Scarlet', 'Mr. Green' ] ]
 
-x[2 + 2] = true;
+game.murder = "???";
 
-console.log(box); // { '0': 'meow', material: 'cardboard', '^&*': 'testing 123' }
+game["weapons"] = ["lasers","angry cats","dish soap"];
 
-console.log(test); // testing 123
+game.name = [];
 
-console.log(x); // { '4': true }
+game.name[0] = "Miss Scarlet";
 
-var obj = {};
+game.name.push("Mr. Green");
 
-obj[function(){}] = false;
-
-console.log(obj);
- 
-console.log(typeof Object.keys(obj));  // object
-
-console.log(Object.keys(obj)[0]);  // function(){}
-
-console.log(typeof Object.keys(obj)[0]);  // string
-
-obj[(function(){return 3;})()] = false;
-
-console.log(obj); // { '3': false, 'function(){}': false }
-
-console.log((function(){return 3;})()); // 3
-
-var y = {};
-
-console.log(y.length); // undefined
-
-var z = [];
-
-console.log(z.length); // 0
-
-z.name = "Meow";
-
-console.log(z.length); // 0 - Key Value pair is not accounted in the indices of the array
-
-console.log(z); // [ name: 'Meow' ] - even though length is 0, z has the property "name": "Meow"
-
-z.push((function(){return "a";})())
-
-console.log(z); // [ 'a', name: 'Meow' ]
-
-console.log(z.length); // 1
-
-z[10] = "hello";
-
-console.log(z); // [ 'a', <9 empty items>, 'hello', name: 'Meow' ] - 9 empty elements.
-
-console.log(z.length); // 11 - Even though there are 12 elements, the Key value Pair name: 'Meow' is ignored.
+console.log(game);
