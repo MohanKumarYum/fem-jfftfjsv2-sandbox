de4b3a362e0e5ddde5a374b6962944e1141df954
Mohan Kumar <MohanKumarYum@Gmail.com>
2018-10-28 07:53:48 +1100

    Using both _.each & _.map functions.

--- sandbox.js
+++ sandbox.js
@@ -1,9 +1,47 @@
-var _ = require("underscore");
+let _ = require("underscore");
+const suspects = ["Miss scarlet", "Colonel Mustard", "Mr. White"];
 
-const weapons = ["candlestick", "lead pipe", "revolver"];
-
-const broken = function(name) {
-  return `broken ${name}`;
+function CreateSuspectsObjects(name) {
+  return {
+    name: name,
+    color: name.split(' ')[1],
+    speak() {
+      console.log(`my name is ${name}`);
+    },
+  };
 };
 
-console.log(_.map(weapons, broken));
+let suspectsList = _.map(suspects, function (name) {
+  return CreateSuspectsObjects(name);
+});
+
+console.log(suspectsList);
+
+// Result
+
+// [{
+//     name: 'Miss scarlet',
+//     color: 'scarlet',
+//     speak: [Function: speak]
+//   },
+//   {
+//     name: 'Colonel Mustard',
+//     color: 'Mustard',
+//     speak: [Function: speak]
+//   },
+//   {
+//     name: 'Mr. White',
+//     color: 'White',
+//     speak: [Function: speak]
+//   }
+// ]
+
+_.each(suspectsList, function (suspect) {
+  suspect.speak();
+});
+
+// Result
+
+// my name is Miss scarlet
+// my name is Colonel Mustard
+// my name is Mr. White
