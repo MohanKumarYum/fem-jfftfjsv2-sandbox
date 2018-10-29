2ccfd1e9a9ec78927b368627bcfcb3cbc1bf9e4e
Mohan Kumar <MohanKumarYum@Gmail.com>
2018-10-29 12:17:50 +1100

    Declare and invoke _.filter function.

--- sandbox.js
+++ sandbox.js
@@ -1,6 +1,96 @@
+// Data to be filtered.
+
+const videoData = [
+  {
+      name: 'Miss Scarlet',
+      present: true,
+      rooms: [
+          {kitchen: false},
+          {ballroom: false},
+          {conservatory: false},
+          {'dining room': false},
+          {'billiard room': false},
+          {library: false}
+      ]
+  },
+  {
+      name: 'Mrs. White',
+      present: false,
+      rooms: [
+          {kitchen: false},
+          {ballroom: false},
+          {conservatory: false},
+          {'dining room': false},
+          {'billiard room': false},
+          {library: false}
+      ]
+  },
+  {
+      name: 'Reverend Green',
+      present: true,
+      rooms: [
+          {kitchen: false},
+          {ballroom: false},
+          {conservatory: false},
+          {'dining room': false},
+          {'billiard room': false},
+          {library: false}
+      ]
+  },
+  {
+      name: 'Rusty',
+      present: false,
+      rooms: [
+          {kitchen: false},
+          {ballroom: false},
+          {conservatory: false},
+          {'dining room': false},
+          {'billiard room': false},
+          {library: false}
+      ]
+  },
+  {
+      name: 'Colonel Mustard',
+      present: true,
+      rooms: [
+          {kitchen: false},
+          {ballroom: false},
+          {conservatory: false},
+          {'dining room': false},
+          {'billiard room': false},
+          {library: false}
+      ]
+  },
+  {
+      name: 'Professor Plum',
+      present: true,
+      rooms: [
+          {kitchen: false},
+          {ballroom: false},
+          {conservatory: false},
+          {'dining room': false},
+          {'billiard room': false},
+          {library: false}
+      ]
+  }
+];
+
+// declare underscore object.
 var _ = {};
 
-const suspects = ["Miss scarlet", "Colonel Mustard", "Mr. White"];
+
+_.filter = function (list, callback) {
+  // Array to be returned.
+  var mappedArray = [];
+  _.each(list, function (val, index, list) {
+    if (callback(val, index, list)){
+      mappedArray.push(list[index]);
+    };
+  });
+  return mappedArray;
+};
+
+// Define _.each function.
 
 _.each = function (list, callback) {
   if (Array.isArray(list)) {
@@ -16,83 +106,28 @@ _.each = function (list, callback) {
   }
 };
 
-// _.each(suspects, function(name){CreateSuspectsObjects(name)});
-
-
-// Result
-// Miss scarlet
-// Colonel Mustard
-// Mr. White
-
-
-// version 2 - using another function _.each() within the _.map function.
-
-_.map = function (list, callback) {
-  var mappedArray = [];
-  _.each(list, function(val, index, list){
-  mappedArray.push(callback(val, index, list))});
-  return mappedArray;
-};
-
-//Result for Version 2 with _.each within the _.map function.
-
-// [{
-//   name: 'Miss scarlet',
-//   color: 'scarlet',
-//   speak: [Function: speak]
-// },
-//   {
-//     name: 'Colonel Mustard', color: 'Mustard',
-//     speak: [Function: speak]
-//   }, { name: 'Mr. White', color: 'White', speak: [Function: speak] }]
-
-// version 1
-// _.map = function (list, callback) {
-//   var mappedArray = [];
-
-//   if (Array.isArray(list)) {
-//     for (let index = 0; index < list.length; index++) {
-//       mappedArray.push(callback(list[index], index, list));
-//     }
-//   } else {
-//     for (const key in list) {
-//       mappedArray.push(callback(list[key], key, list));
-//     }
-//   }
-//   return mappedArray;
-// };
-
-
-const callback = function CreateSuspectsObjects(name) {
-  return {
-    name: name,
-    color: name.split(' ')[1],
-    speak() {
-      console.log(`my name is ${name}`);
-    },
-  };
-};
-
-let suspectsList = _.map(suspects, callback);
+let suspectsList = _.filter(videoData, function(suspectObject){
+  return suspectObject.present;
+});
 
 console.log(suspectsList);
 
 // Result
 
-// [{
-//   name: 'Miss scarlet',
-//   color: 'scarlet',
-//   speak: [Function: speak]
-// },
-// {
-//   name: 'Colonel Mustard',
-//   color: 'Mustard',
-//   speak: [Function: speak]
-// },
-// { name: 'Mr. White', color: 'White', speak: [Function: speak] }]
+// SuspectObject with present property is true. -  mappedArray.push(list[index].name);
+
+// [ 'Miss Scarlet',  'Reverend Green',
+//   'Colonel Mustard',
+//   'Professor Plum' ]
+
+// Result - mappedArray.push(val)
 
 
-// console.log(_.map([1, 2, 3], function (val) { return val + 1; }));
-
-// Result 
-// [ 2, 3, 4 ]
+// [ { name: 'Miss Scarlet',    present: true,
+//     rooms:     [ [Object], [Object], [Object], [Object], [Object], [Object] ] },
+//   { name: 'Reverend Green',    present: true,
+//     rooms:     [ [Object], [Object], [Object], [Object], [Object], [Object] ] },
+//   { name: 'Colonel Mustard',
+//     present: true,    rooms:     [ [Object], [Object], [Object], [Object], [Object], [Object] ] },  { name: 'Professor Plum',    present: true,
+//     rooms:
+//      [ [Object], [Object], [Object], [Object], [Object], [Object] ] } ]
